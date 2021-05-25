dd584faf8360f13b4e8f0b3636e5c4ab7b7425d4
hphilamore <hemmaphilamore@gmail.com>
2021-05-25 11:49:35 +0100

    bug fix: new array for push button inputs. seperate functions for button input, 4 and 6 SMA outputs

--- SMA_tentacle_control.ino
+++ SMA_tentacle_control.ino
@@ -14,15 +14,15 @@
 
 
 
- Hardware setup: Transistors controlling output to SMAs (repeat for pins D3/ D5/ D9/ D11  and SMAs a / b / c / d)
+ Hardware setup: Transistors controlling output to SMAs (repeat for pins D3/ D5/ D9/ D11 [ and D6/ D10 ] and and SMAs a / b / c / d [ and e/ f ])
  
- |---- D3 / D5 / D9 / D11 
+ |---- D3 / D5 / D9 / D11 // D6 / D10
  |
  |---- Gate BC547C (NPN General Purpose Transistor) 
  |
  |---- GND
 
-
+            
 
  |---- Vcc
  |
@@ -31,10 +31,10 @@
 
  |---- Emitter BC547C (NPN General Purpose Transistor)
  |
- |---- SMA a / b / c / d positive terminal 
+ |---- SMA a / b / c / d // e / f positive terminal 
 
 
- |---- SMA a & b & c & d negative terminal (terminals for all SMAs connected together) 
+ |---- SMA a & b & c & d // e & f negative terminal (terminals for all SMAs connected together) 
  |
  |---- GND 
  
@@ -61,7 +61,7 @@
  |---- Vcc 
  */
  
-//const int BUFFER_SIZE_tail = 3;
+const int BUFFER_SIZE_tail = 3;
 //const uint8_t pins_buttons[] = {A1,A2,A3};
 
 //const int n_SMAs_tail = 2;
@@ -70,10 +70,19 @@
 //const int n_SMAs_tail = 3;
 //const int pins_tail[] = {5, 9, 11};
 
-const int BUFFER_SIZE_tail = 4;
+//const int BUFFER_SIZE_tail = 4;
+const int n_buttons = 4;
 const uint8_t pins_buttons[] = {A1,A2,A3,A4};
+
+
+// const int pins_tail[] = {3, 5, 9, 11};
+// const int pins_tail[] = {5, 6, 9, 11};
+//const int pins_tail[] = {3, 5, 6, 11};
 const int n_SMAs_tail = 4;
-const int pins_tail[] = {3, 5, 9, 11};
+const int pins_tail[] = {3, 5, 9, 11}; 
+
+//const int n_SMAs_tail = 6;
+//const int pins_tail[] = {3, 5, 6, 9, 10, 11};
 
 class SMA_tentacle {
   
@@ -83,6 +92,7 @@ class SMA_tentacle {
     int *_out_pins;
     char *_buffer;
     int N_SMAs;
+    int N_buttons;
     int n_bytes;
     int BUFFER_SIZE;
     int error;
@@ -100,16 +110,20 @@ class SMA_tentacle {
     
   public:
     // constructor
-    SMA_tentacle(int out_pins[], const int N_SMAs, const int BUFFER_SIZE, uint8_t in_pins[]) {
+    SMA_tentacle(int out_pins[], const int N_SMAs, const int BUFFER_SIZE, uint8_t in_pins[], const int N_buttons) {
       this->N_SMAs = N_SMAs; 
       this-> BUFFER_SIZE = BUFFER_SIZE;
+      this-> N_buttons = N_buttons;
       
-      _out_pins = (int *)malloc(sizeof(int) * N_SMAs); 
-      _buffer = (char *)malloc(sizeof(char) * BUFFER_SIZE);        // array to store serial inputs
-      _in_pins = (uint8_t *)malloc(sizeof(uint8_t) * BUFFER_SIZE); // array to store physical inputs same length as buffer (for testing)
+      _out_pins = (int *)malloc(sizeof(int) * N_SMAs);         
+      _buffer = (char *)malloc(sizeof(char) * BUFFER_SIZE);      // array to store serial inputs
+      _in_pins = (uint8_t *)malloc(sizeof(uint8_t) * N_buttons); // array to store physical inputs same length as buffer (for testing)
       
       for(int i=0; i<N_SMAs; i++){
         _out_pins[i] = out_pins[i];
+      }
+
+      for(int i=0; i<N_buttons; i++){
         _in_pins[i] = in_pins[i];
       }
       
@@ -120,26 +134,58 @@ class SMA_tentacle {
       for(int i=0; i<N_SMAs; i++){
         pinMode(_out_pins[i], OUTPUT);
         digitalWrite(_out_pins[i], LOW);
+      } 
+      pinMode(LED_pin, OUTPUT);
+      digitalWrite(LED_pin, LOW);
+      for(int i=0; i<N_buttons; i++){
         pinMode(_in_pins[i], INPUT);
-        pinMode(LED_pin, OUTPUT);
-        digitalWrite(LED_pin, LOW);
-      }      
+      }     
     }
     
-    void button_control_SMA(){
+    void button4_control_SMA4(){
        // use push buttons to control tentacle
-       for(int i=0; i<N_SMAs; i++){
+       for(int i=0; i<N_buttons; i++){
         if(digitalRead(_in_pins[i]) == HIGH){ 
           digitalWrite(_out_pins[i], HIGH); 
-          Serial.print("high");
-          }
+        }
+          
         else{
          digitalWrite(_out_pins[i], LOW);
-         Serial.print("low"); 
+         Serial.print("low");          
          } 
-       }
+       
        Serial.println("");
+       }
     }
+
+
+  void button4_control_SMA6(){
+        // use push buttons to control tentacle
+        // LEFT
+        if(digitalRead(_in_pins[0]) == HIGH){ 
+            for(int i=0; i<3; i++){
+              digitalWrite(_out_pins[i], HIGH);
+            }
+          }
+          else{
+            for(int i=0; i<3; i++){
+              digitalWrite(_out_pins[i], LOW);
+            }
+          }
+
+          // RIGHT
+          if(digitalRead(_in_pins[1]) == HIGH){ 
+            for(int i=3; i<6; i++){
+              digitalWrite(_out_pins[i], HIGH);
+            }
+          }
+          else{
+            for(int i=3; i<6; i++){
+              digitalWrite(_out_pins[i], LOW);
+            }
+          }
+    }
+
 
     void serial_control_SMA(){
       // control tentacle over serial by simply choosing to move left or right based on value received 
@@ -147,15 +193,64 @@ class SMA_tentacle {
         n_bytes = Serial.readBytes(_buffer, BUFFER_SIZE);
           //if (_buffer[0] < 50) {               // if number sent over serial < 50 move tentacle left 
           if (_buffer[0] < _buffer[1]) {         // if number sent over serial < 50 move tentacle left
-            digitalWrite(_out_pins[0], LOW);
-            digitalWrite(_out_pins[1], HIGH); 
+            for(int i=0; i<(N_SMAs/2); i++){digitalWrite(_out_pins[i], LOW);}
+            for(int i=(N_SMAs/2); i<N_SMAs; i++){digitalWrite(_out_pins[i], HIGH);} 
+//            digitalWrite(_out_pins[0], LOW);
+//            digitalWrite(_out_pins[1], LOW);
+//            digitalWrite(_out_pins[2], HIGH); 
+//            digitalWrite(_out_pins[3], HIGH); 
             }
           else{                                  // otherwise move right 
-            digitalWrite(_out_pins[0], HIGH);
-            digitalWrite(_out_pins[1], LOW); 
+            for(int i=0; i<(N_SMAs/2); i++){digitalWrite(_out_pins[i], HIGH);}
+            for(int i=(N_SMAs/2); i<N_SMAs; i++){digitalWrite(_out_pins[i], LOW);} 
+//            digitalWrite(_out_pins[0], HIGH);
+//            digitalWrite(_out_pins[1], HIGH); 
+//            digitalWrite(_out_pins[2], LOW);
+//            digitalWrite(_out_pins[3], LOW); 
             }
         }
     }
+
+
+//    void serial_control_SMA(){
+//      // control tentacle over serial by simply choosing to move left or right based on value received 
+//      if (Serial.available()) {          
+//        n_bytes = Serial.readBytes(_buffer, BUFFER_SIZE);
+//          //if (_buffer[0] < 50) {               // if number sent over serial < 50 move tentacle left 
+//          if (_buffer[0] < _buffer[1]) {         // if number sent over serial < 50 move tentacle left
+//            for(int i=1; i<(n_SMAs_tail+1/2); i++){digitalWrite(_out_pins[i], LOW);}
+//            for(int i=(n_SMAs_tail+1/2); i<n_SMAs_tail; i++){digitalWrite(_out_pins[i], HIGH);} 
+//            }
+//          else{                                  // otherwise move right 
+//            for(int i=1; i<(n_SMAs_tail+1/2); i++){digitalWrite(_out_pins[i], HIGH);}
+//            for(int i=(n_SMAs_tail+1/2); i<n_SMAs_tail; i++){digitalWrite(_out_pins[i], LOW);} 
+//            }
+//        }
+//    }
+
+//    void serial_control_SMA(){
+//      // control tentacle over serial by simply choosing to move left or right based on value received 
+//      if (Serial.available()) {          
+//        n_bytes = Serial.readBytes(_buffer, BUFFER_SIZE);
+//          //if (_buffer[0] < 50) {               // if number sent over serial < 50 move tentacle left 
+//          if (_buffer[0] < _buffer[1]) {         // if number sent over serial < 50 move tentacle left
+//            for(int i=0; i<(n_SMAs_tail/2); i++){
+//              digitalWrite(_out_pins[i], LOW);
+//              }
+//            for(int i=n_SMAs_tail/2; i<(n_SMAs_tail); i++){
+//              digitalWrite(_out_pins[i], HIGH);
+//              }
+//            }
+//          else{                                  // otherwise move right 
+//            for(int i=0; i<(n_SMAs_tail/2); i++){
+//              digitalWrite(_out_pins[i], HIGH);
+//              }
+//            for(int i=n_SMAs_tail/2; i<(n_SMAs_tail); i++){
+//              digitalWrite(_out_pins[i], LOW);
+//              }
+//            }
+//        }
+//    }
 
 
     void PID_serial_control_SMA(){
@@ -234,14 +329,22 @@ class SMA_tentacle {
 }; 
 
 
-SMA_tentacle sma_tentacle(pins_tail, n_SMAs_tail, BUFFER_SIZE_tail, pins_buttons);
+SMA_tentacle sma_tentacle(pins_tail, n_SMAs_tail, BUFFER_SIZE_tail, pins_buttons, n_buttons);
 
 void setup() {
   Serial.begin(57600);
 }
 
 void loop() {
-    sma_tentacle.button_control_SMA();
-    //sma_tentacle.serial_control_SMA();
+//  if(n_SMAs_tail == 4){
+//    sma_tentacle.button4_control_SMA4();
+//  }
+//
+// else if(n_SMAs_tail == 6){
+//    sma_tentacle.button4_control_SMA6();
+//  }
+  
+    //sma_tentacle.button_control_SMA();
+    sma_tentacle.serial_control_SMA();
     //sma_tentacle.PID_serial_control_SMA();
 }
